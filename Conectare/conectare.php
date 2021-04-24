32f9f54867513cbe748e2c62206133aebea9c52a
MarcusBanea <marcus.banea27@yahoo.com>
2021-04-24 21:13:40 +0300

    Sanitize

--- Conectare/conectare.php
+++ Conectare/conectare.php
@@ -16,6 +16,32 @@ function connectToDatabase() {
     }
 }
 
+/*
+function sanitize($data)
+{ // testare si curartire pt. SQL injection si XSS
+    // De asemenea poate sa fie folosit si pentru getting POST/GET variables
+    //$_POST = sanitize($_POST);
+    //$_GET  = sanitize($_GET);
+    if (is_array($data))
+        foreach($data as $var=>$val) {
+            $output[$var] = sanitize($val);
+        }
+    else
+    {
+        // remove whitespaces (not a must though)
+        $data = trim($data);
+        // apply stripslashes if magic_quotes_gpc is enabled
+        if(get_magic_quotes_gpc())
+        {	$data = stripslashes($data); }
+        // curatire p. atacuri XSS cros site scripting
+        $data  = cleanInput($data);
+        // a mySQL connection is required before using this function
+        // transforma caracterele speciale in escape (\x00, \n, \r, \, ', " and \x1a.)
+        $data = mysql_real_escape_string($data);
+    }
+    return $data;
+}
+*/
 
 function test_input($data): string
 { // testare si curatire data intrare
